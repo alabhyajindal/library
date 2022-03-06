5ca06f90be64706a51a08facfd320c461fd95caf
Alabhya Jindal <alabhya10@gmail.com>
2022-03-06 19:01:36 +0530

    unique book titles in featured

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-// (function (d) {
-//   var w = d.documentElement.offsetWidth,
-//     t = d.createTreeWalker(d.body, NodeFilter.SHOW_ELEMENT),
-//     b;
-//   while (t.nextNode()) {
-//     b = t.currentNode.getBoundingClientRect();
-//     if (b.right > w || b.left < 0) {
-//       t.currentNode.style.setProperty('outline', '1px dotted red', 'important');
-//       console.log(t.currentNode);
-//     }
-//   }
-// })(document);
+const recommended = document.querySelector('.recommended');
+const featured = document.querySelector('.featured');
+const books = document.querySelector('.books');
+const rBooks = document.querySelector('.r-books');
+const heading = document.querySelector('.book-heading');
+const genre = document.querySelector('.genre-filter');
+
+recommended.addEventListener('click', () => {
+  books.style.display = 'none';
+  rBooks.style.display = 'block';
+  heading.textContent = 'Recommendations';
+  featured.classList.remove('active-link');
+  recommended.classList.add('active-link');
+  genre.textContent = 'Fiction';
+});
+
+featured.addEventListener('click', () => {
+  books.style.display = 'block';
+  rBooks.style.display = 'none';
+
+  heading.textContent = 'Featured';
+  recommended.classList.remove('active-link');
+  featured.classList.add('active-link');
+  genre.textContent = 'Nonfiction';
+});
